926ab2caaf0551399f3263c62a67958b34d2f08a
Adil Madhani <adilmadhani@Adils-MacBook-Pro.local>
2019-06-23 14:50:16 -0400

    changed index to index_hello, added index to progress course

--- part1/src/index.js
+++ part1/src/index.js
@@ -1,24 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-const Hello = (props) => {
+const App = (props) => {
+  const {counter} = props
   return (
-    <div>
-      <p>Hello {props.name}, you are {props.age} years old</p>
-    </div>
+    <div>{counter}</div>
   )
 }
 
-const App = () => {
-  const name="Peter"
-  const age=10
-  return(
-    <div>
-      <h1>Greetings</h1>
-      <Hello name="Joel" age={26+10} />
-      <Hello name={name} age={age} />
-    </div>
-  )
-}
+let counter = 1
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(
+  <App counter={counter} />, 
+  document.getElementById('root')
+)
