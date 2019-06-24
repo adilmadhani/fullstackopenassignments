e2c745084dff09c645c3d7a79da86727813ff7bb
Adil Madhani <adilmadhani@Adils-MacBook-Pro.local>
2019-06-24 02:45:32 -0400

    End of part 1c

--- part1/src/index.js
+++ part1/src/index.js
@@ -1,16 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
 const App = (props) => {
-  const {counter} = props
+  const [ counter, setCounter ] = useState(0)
+  const setToValue = (value) => setCounter(value)
+
   return (
-    <div>{counter}</div>
+    <div>
+      <Display counter={counter} />
+      <Button onClick={() => setToValue(counter + 1)}
+      text="plus"
+      />
+      <Button onClick={() => setToValue(counter - 1)}
+      text="minus"
+      />
+      <Button onClick={() => setToValue(0)}
+      text="zero"
+      />
+    </div>
   )
 }
 
-let counter = 1
+const Display = ({ counter }) => <div>{counter}</div>
+
+const Button = ({onClick, text}) => (
+  <button onClick={onClick}>
+    {text}
+  </button>
+)
 
 ReactDOM.render(
-  <App counter={counter} />, 
+  <App />, 
   document.getElementById('root')
 )
